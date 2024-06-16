2815a03e7e3881ee83c6da6a1f04510306bd9ab2
RaphaCosil <Raphael.Cosil@gmail.com>
2024-06-16 00:08:00 -0300

    Add - Custom dialog to get information to create posts

--- app/src/main/java/com/example/retrofittest/view/MainActivity.kt
+++ app/src/main/java/com/example/retrofittest/view/MainActivity.kt
@@ -1,40 +1,114 @@
 package com.example.retrofittest.view
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofittest.R
 import com.example.retrofittest.RetrofitInstance
 import com.example.retrofittest.appModule
+import com.example.retrofittest.databinding.ActivityMainBinding
 import com.example.retrofittest.domain.repository.PostsRepositoryImpl
 import com.example.retrofittest.domain.usecase.*
 import com.example.retrofittest.model.PostData
-import com.example.retrofittest.viewmodel.PostsViewModel
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
+import retrofit2.HttpException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recycleAdapter: RecycleAdapter
+    private lateinit var binding: ActivityMainBinding
+    private val retrofitRepository = PostsRepositoryImpl(RetrofitInstance.api)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         startKoin {
             androidContext(this@MainActivity)
             modules(appModule)
         }
-        val retrofitRepository = PostsRepositoryImpl(RetrofitInstance.api)
-        val userViewModel = PostsViewModel(
-            GetPostsUseCase(retrofitRepository),
-            GetPostUseCase(retrofitRepository),
-            CreatePostUseCase(retrofitRepository),
-            UpdatePostUseCase(retrofitRepository),
-            PatchPostUseCase(retrofitRepository),
-            DeletePostUseCase(retrofitRepository)
-        )
 
-        userViewModel.getPosts()
-        userViewModel.getPost("1")
-        userViewModel.createPost(PostData(1, "title", "body", 1))
-        userViewModel.updatePost("1", PostData(1, "title", "body", 1))
-        userViewModel.patchPost("1", PostData(1, "title", "body", 1))
-        userViewModel.deletePost("1")
+        setupRecyclerView()
+        lifecycleScope.launchWhenCreated {
+            val response = try {
+                GetPostsUseCase(retrofitRepository).invoke()
+            } catch(e: IOException) {
+                Log.e(TAG, "IOException, you might not have internet connection")
+                return@launchWhenCreated
+            } catch(e: HttpException) {
+                Log.e(TAG, "HttpException, unexpected response")
+                return@launchWhenCreated
+            }
+            if(response.isSuccessful && response.body() != null) {
+                recycleAdapter.postsList = response.body()!!
+            } else {
+                Log.e(TAG, "Response not successful")
+            }
+        }
+        binding.floatingActionButton.setOnClickListener {
+            showPostDialog(this)
+        }
+
+
+    }
+    private fun setupRecyclerView() = binding.recyclerView.apply {
+        recycleAdapter = RecycleAdapter()
+        adapter = recycleAdapter
+        addItemDecoration(SpaceItemDecoration(resources.getDimension(R.dimen.recycler_view_item_space).toInt()))
+        layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+    private fun showPostDialog(context: Context) {
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_layout, null)
+        val builder = AlertDialog.Builder(context, R.style.CustomDialogTheme)
+            .setView(dialogView)
+        val dialog = builder.create()
+        dialog.show()
+
+        val buttonSubmit = dialogView.findViewById<Button>(R.id.buttonSubmit)
+        val buttonCancel = dialogView.findViewById<Button>(R.id.buttonCancel)
+        val editTextTitle = dialogView.findViewById<EditText>(R.id.editTextTitle)
+        val editTextBody = dialogView.findViewById<EditText>(R.id.editTextBody)
+
+        buttonSubmit.setOnClickListener {
+            val title = editTextTitle.text.toString()
+            val body = editTextBody.text.toString()
+            submitPost(title, body)
+            dialog.dismiss()
+        }
+
+        buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+
+
+    private fun submitPost(title: String, body: String) {
+        lifecycleScope.launch {
+            val response = try {
+                CreatePostUseCase(retrofitRepository).invoke(PostData(id = 101,title = title, body = body, userId = 1))
+            } catch(e: IOException) {
+                Log.e(TAG, "IOException, you might not have internet connection")
+                return@launch
+            } catch(e: HttpException) {
+                Log.e(TAG, "HttpException, unexpected response")
+                return@launch
+            }
+            if(response.isSuccessful) {
+                Toast.makeText(this@MainActivity, "Post created successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(TAG, "Response not successful")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/retrofittest/view/RecycleAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.retrofittest.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofittest.databinding.ItemRecycleBinding
+import com.example.retrofittest.model.PostData
+
+class RecycleAdapter : RecyclerView.Adapter<RecycleAdapter.PostViewHolder>() {
+    inner class PostViewHolder(val binding: ItemRecycleBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<PostData>() {
+        override fun areItemsTheSame(oldItem: PostData, newItem: PostData): Boolean {
+            return oldItem.id == newItem.id
+        }
+        override fun areContentsTheSame(oldItem: PostData, newItem: PostData): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+    var postsList: List<PostData>
+        get() = differ.currentList
+        set(value) { differ.submitList(value) }
+
+    override fun getItemCount() = postsList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        return PostViewHolder(ItemRecycleBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.binding.apply {
+            val posts = postsList[position]
+            textViewTitle.text = posts.title
+            textViewBody.text = posts.body
+        }
+    }
+}
